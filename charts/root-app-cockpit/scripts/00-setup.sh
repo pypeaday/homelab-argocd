b077c3510ce3602e92ac5ecbd7cf77369321dd30
pypeaday <github@pype.aleeas.com>
2024-03-19 12:46:42 -0500

    oof

--- charts/root-app-cockpit/scripts/00-setup.sh
+++ charts/root-app-cockpit/scripts/00-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-helm template ~/personal/argocd-helm/charts/root-app/ | kubectl apply -f -
-# helm template ~/personal/argocd-helm/charts/root-app/ | kubectl apply -v=8 -f -
+helm template ~/personal/argocd-helm/charts/root-app-cockpit/ | kubectl apply -f -
+# helm template ~/personal/argocd-helm/charts/root-app-cockpit/ | kubectl apply -v=8 -f -
